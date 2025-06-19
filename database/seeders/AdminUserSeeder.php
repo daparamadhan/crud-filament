6d462d15f39d6e5c61cbe9cdfe96f32e27c5aaf5
Daffa Ramadhan <daparamdnnn@gmail.com>
2025-06-19 11:09:57 +0700

    perubahan

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User; // ✅ Tambahkan baris ini
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate([
+            'email' => 'admin@example.com',
+        ], [
+            'name' => 'Admin',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
